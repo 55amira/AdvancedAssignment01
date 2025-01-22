967d5db28fb2c95846f063219ceba8af9d61aaf2
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-22 22:30:34 +0200

    Generics,Ex : SWAP

---
+++ Demo/Healper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Healper/*<T>*/
+    {
+        public static void  SWAP<T>(ref T X, ref T Y)
+        {
+            T temp;
+            temp = X;
+            X = Y;
+            Y = temp;
+        }
+
+        public static void Print <T>( T data)
+        {
+            Console.WriteLine(data);
+        }
+
+        #region Non Genaric
+        //    public static void SWAP(ref int X ,ref int Y)
+        //    {
+        //        int temp;
+        //        temp = X;
+        //        X = Y;
+        //        Y = temp;
+        //    }
+
+        //    public static void SWAP(ref double X, ref double Y)
+        //    {
+        //        double temp;
+        //        temp = X;
+        //        X = Y;
+        //        Y = temp;
+        //    }
+
+        //    public static void SWAP(ref Point X, ref Point Y)
+        //    {
+        //        Point temp;
+        //        temp = X;
+        //        X = Y;
+        //        Y = temp;
+        //    } 
+        #endregion
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,87 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generic Ex : SWAP
+            //int A = 4;
+            //int B = 5;
+
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper.SWAP( ref A, ref B );
+
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+
+            //double L = 1.4;
+            //double K = 2.5;
+
+            //Console.WriteLine($"L = {L}");
+            //Console.WriteLine($"K = {K}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper.SWAP(ref L, ref K);
+
+            //Console.WriteLine($"L = {L}");
+            //Console.WriteLine($"K = {K}");
+
+            //Point P01 = new Point(1, 1);
+            //Point P02 = new Point(2, 2);
+
+            //Console.WriteLine($"P01 = {P01}");
+            //Console.WriteLine($"P02 = {P02}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper.SWAP(ref P01, ref P02);
+
+            //Console.WriteLine($"P01 = {P01}");
+            //Console.WriteLine($"P02 = {P02}");
+
+            /*****************************************/
+
+            //int A = 4;
+            //int B = 5;
+
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper.SWAP(ref A, ref B);
+
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+
+            //double L = 1.4;
+            //double K = 2.5;
+
+            //Console.WriteLine($"L = {L}");
+            //Console.WriteLine($"K = {K}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper.SWAP(ref L, ref K);
+
+            //Console.WriteLine($"L = {L}");
+            //Console.WriteLine($"K = {K}");
+
+            //Point P01 = new Point(1, 1);
+            //Point P02 = new Point(2, 2);
+
+            //Console.WriteLine($"P01 = {P01}");
+            //Console.WriteLine($"P02 = {P02}");
+
+            //Console.WriteLine("*********After SWAP************");
+
+            //Healper/*<T>*/.SWAP(ref P01, ref P02);
+
+            //Console.WriteLine($"P01 = {P01}");
+            //Console.WriteLine($"P02 = {P02}"); 
+            #endregion
         }
     }
 }
